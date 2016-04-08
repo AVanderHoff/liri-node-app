1dccdc7a2c2b457ee1621bff11e9f4cf29353609
Andrew VanderHoff <andrewvanderhoff@gmail.com>
2016-04-08 18:49:56 -0400

    unignored key.js

---
+++ keys.js
@@ -0,0 +1,8 @@
+//console.log('this is loaded');
+
+exports.twitterKeys = {
+  consumer_key: 'gVIAyXtLTgzhmZ5SczUGG4xye',
+  consumer_secret: 'MKhsWE3xgheJPx78hpse4TykJG54IPyioaP16ql7uvNJxfxTbV',
+  access_token_key: '4831423355-Za48Nol0A5ZGifY3BNFYbbNoJ1Ap3o6KcUFHHx5',
+  access_token_secret: 'nJunMOrOSG50TN2yJZiia0sHDacoa2ulZEO4ammY7owuY',
+}
